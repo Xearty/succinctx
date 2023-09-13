c2f2fdd4daac3802621b7a6e29ae3d8ff6ff1a16
Xearty <xeartybg@gmail.com>
2023-09-13 21:10:53 +0300

    Implement subarray comparison by using a random linear combination

--- plonky2x/src/frontend/eth/mpt/rlc.rs
+++ plonky2x/src/frontend/eth/mpt/rlc.rs
@@ -6,17 +6,41 @@ use plonky2::iop::challenger::RecursiveChallenger;
 use super::generators::SubarrayEqualGenerator;
 use crate::prelude::{BoolVariable, ByteVariable, CircuitBuilder, PlonkParameters, Variable};
 
-// Checks that a[a_offset:a_offset+len] = b[b_offset:b_offset+len]
-pub fn subarray_equal(a: &[u8], a_offset: usize, b: &[u8], b_offset: usize, len: usize) -> u8 {
-    for i in 0..len {
-        if a[a_offset + i] != b[b_offset + i] {
-            return 0;
-        }
-    }
-    1
-}
+// @TODO(xearty): put this elsewhere
+const MAX_ELEMENTS: u64 = 64;
 
 impl<L: PlonkParameters<D>, const D: usize> CircuitBuilder<L, D> {
+    fn commit_subarray(
+        &mut self,
+        arr: &[ByteVariable],
+        offset: Variable,
+        len: Variable,
+        random_variables: &[Variable; MAX_ELEMENTS]
+    ) -> Variable {
+        let one = self.one();
+        let end_idx = bulder.add(offset, len);
+        let mut is_within_subarray = BoolVariable(self.zero());
+        let mut commitment = self.zero();
+
+        let idx_target = self.zero();
+        for idx in 0..MAX_ELEMENTS {
+            // is_within_subarray is one if idx is in the range [offset..offset+len]
+            let is_at_start_idx = self.is_equal(idx_target, offset);
+            is_within_subarray = self.add(is_within_subarray, at_start_idx);
+            let is_at_end_idx = self.is_equal(idx_target, end_idx);
+            is_within_subarray = self.sub(is_within_subarray, is_at_end_idx);
+
+            // if in range, include the byte, multiplied by a random value
+            let subarray_idx = self.mul(self.sub(idx_target, offset), is_within_subarray);
+            let random_value_if_in_range = self.mul(is_within_subarray, self.mul(a[idx], random_variables[idx]));
+            commitment = self.add(commitment, random_value_if_in_range);
+
+            idx_target = self.add(idx_target, one);
+        }
+
+        commitment
+    }
+
     #[allow(unused_variables, dead_code)]
     pub fn subarray_equal(
         &mut self,
@@ -26,7 +50,14 @@ impl<L: PlonkParameters<D>, const D: usize> CircuitBuilder<L, D> {
         b_offset: Variable,
         len: Variable,
     ) -> BoolVariable {
-        todo!();
+        let mut challenger = RecursiveChallenger::<L::Field, PoseidonHash, D>::new(&mut self.api);
+        let challenger_seed = Vec::new();
+        challenger.observe_elements(&challenger_seed);
+        let random_variables = challenger.get_n_challenges(&mut self.api, MAX_ELEMENTS);
+
+        let commitment_for_a = self.commit_subarray(a, a_offset, len, random_variables);
+        let commitment_for_b = self.commit_subarray(b, b_offset, len, random_variables);
+        self.is_equal(commitment_for_a, commitment_for_b)
     }
 
     #[allow(unused_variables, dead_code)]
@@ -38,29 +69,7 @@ impl<L: PlonkParameters<D>, const D: usize> CircuitBuilder<L, D> {
         b_offset: Variable,
         len: Variable,
     ) {
-        // TODO: instead of using the SubarrayEqualGenerator below that doesn't actually check anything, implement an RLC check here
-        let generator: SubarrayEqualGenerator<L, D> = SubarrayEqualGenerator {
-            a: a.to_vec(),
-            a_offset,
-            b: b.to_vec(),
-            b_offset,
-            len,
-            _phantom: PhantomData::<L>,
-        };
-        self.add_simple_generator(generator);
-
-        // The following methods might be helpful
-        let mut challenger = RecursiveChallenger::<L::Field, PoseidonHash, D>::new(&mut self.api);
-        let challenger_seed = Vec::new(); // TODO: have to "seed" the challenger with some random inputs from the circuit
-        challenger.observe_elements(&challenger_seed);
-
-        let random_variables = challenger.get_n_challenges(&mut self.api, 1);
-        let random_variable = random_variables[0];
-
-        // To convert from a Target to a Variable, just use Variable(my_target) to get a Variable
-
-        // TODO: now compute a commitment to a[a_offset:a_offset+len]
-        // TODO: now compute a commitment to b[b_offset:b_offset+len]
+        self.api.assert_bool(subarray_equal(&a, a_offset, &b, b_offset, len));
     }
 }
 
